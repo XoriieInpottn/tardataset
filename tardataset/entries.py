6d85390c5709ae77e18be4be2304149aec534986
XoriieInpottn <XoriieInpottn@126.com>
2021-03-21 23:19:06 +0800

    bsontar command improved

--- tardataset/entries.py
+++ tardataset/entries.py
@@ -29,10 +29,26 @@ def entry_bsontar():
                     value = f'"{value}"'
                 elif isinstance(value, np.ndarray):
                     value = f'ndarray(dtype={value.dtype}, shape={value.shape})'
+                elif isinstance(value, bytes):
+                    size = _format_size(len(value))
+                    value = f'binary(size={size})'
                 print(f'    "{name}": {value}')
             print('}')
 
     return 0
+
+
+def _format_size(size):
+    _POWER_LABELS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+    n = 0
+    while size >= 1024:
+        if n + 1 >= len(_POWER_LABELS):
+            break
+        size /= 1024
+        n += 1
+    if n == 0:
+        return f'{size}{_POWER_LABELS[n]}'
+    return f'{size:.01f}{_POWER_LABELS[n]}'
 
 
 if __name__ == '__main__':
